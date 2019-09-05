74d37b8a6f9ee61d304f92e594068bc8ceb27b86
Shubhank Khare <shubhank7673@gmail.com>
2019-09-05 12:34:00 +0530

    First commit

---
+++ app/src/main/java/newsExpress/Shubhank7673/tk/MainActivity.java
@@ -0,0 +1,29 @@
+package newsExpress.Shubhank7673.tk;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.widget.Toolbar;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setUpToolbar();
+    }
+    private void setUpToolbar()
+    {
+        drawerLayout = findViewById(R.id.drawer);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.app_name,R.string.app_name);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+}
